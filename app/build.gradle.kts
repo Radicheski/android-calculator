9699930b8026987e4f6533db501b0a943c9c7a9d
Erik Radicheski da Silva <erik.radicheski@gmail.com>
2023-08-12 17:25:18 -0300

    Projeto vazio

---
+++ app/build.gradle.kts
@@ -0,0 +1,42 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    alias(libs.plugins.androidApplication)
+}
+
+android {
+    namespace = "dev.radicheski.calculator"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "dev.radicheski.calculator"
+        minSdk = 10
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_20
+        targetCompatibility = JavaVersion.VERSION_20
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
