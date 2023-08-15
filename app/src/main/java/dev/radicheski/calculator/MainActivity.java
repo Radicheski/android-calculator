b070554b425fe918b9b870f50b02ac9bfa3ed02c
Erik Radicheski da Silva <erik.radicheski@gmail.com>
2023-08-14 21:23:27 -0300

    Adicionadas funções clear, changeSign, percent, setDecimal e appendNumber

--- app/src/main/java/dev/radicheski/calculator/MainActivity.java
+++ app/src/main/java/dev/radicheski/calculator/MainActivity.java
@@ -3,14 +3,24 @@ package dev.radicheski.calculator;
 import android.support.v7.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Function;
 
 public class MainActivity extends AppCompatActivity {
+
+    private double numerator = 0;
+    private double denominator = 1;
+    private boolean decimal = false;
+    private double answer = 0;
 
     private TextView textView;
 
@@ -21,6 +31,23 @@ public class MainActivity extends AppCompatActivity {
             R.id.button0, R.id.button1, R.id.button2, R.id.button3, R.id.button4,
             R.id.button5, R.id.button6, R.id.button7, R.id.button8, R.id.button9
     );
+
+    private final Map<Integer, Runnable> functions = new HashMap<>() {{
+        put(R.id.buttonClear, MainActivity.this::clear);
+        put(R.id.buttonSign, MainActivity.this::changeSign);
+        put(R.id.buttonPercent, MainActivity.this::percent);
+        put(R.id.buttonDecimal, MainActivity.this::setDecimal);
+        put(R.id.button0, () -> appendNumber(0));
+        put(R.id.button1, () -> appendNumber(1));
+        put(R.id.button2, () -> appendNumber(2));
+        put(R.id.button3, () -> appendNumber(3));
+        put(R.id.button4, () -> appendNumber(4));
+        put(R.id.button5, () -> appendNumber(5));
+        put(R.id.button6, () -> appendNumber(6));
+        put(R.id.button7, () -> appendNumber(7));
+        put(R.id.button8, () -> appendNumber(8));
+        put(R.id.button9, () -> appendNumber(9));
+    }};
 
     private final List<Button> BUTTONS = new ArrayList<>(BUTTONS_IDS.size());
 
@@ -39,46 +66,32 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void onClick(View view) {
-        int id = view.getId();
-        if (id == R.id.button0) {
-            textView.setText("0");
-        } else if (id == R.id.button1) {
-            textView.setText("1");
-        } else if (id == R.id.button2) {
-            textView.setText("2");
-        } else if (id == R.id.button3) {
-            textView.setText("3");
-        } else if (id == R.id.button4) {
-            textView.setText("4");
-        } else if (id == R.id.button5) {
-            textView.setText("5");
-        } else if (id == R.id.button6) {
-            textView.setText("6");
-        } else if (id == R.id.button7) {
-            textView.setText("7");
-        } else if (id == R.id.button8) {
-            textView.setText("8");
-        } else if (id == R.id.button9) {
-            textView.setText("9");
-        } else if (id == R.id.buttonClear) {
-            textView.setText("C");
-        } else if (id == R.id.buttonSign) {
-            textView.setText("±");
-        } else if (id == R.id.buttonPercent) {
-            textView.setText("%");
-        } else if (id == R.id.buttonDecimal) {
-            textView.setText(",");
-        } else if (id == R.id.buttonDivision) {
-            textView.setText("÷");
-        } else if (id == R.id.buttonMultiplication) {
-            textView.setText("x");
-        } else if (id == R.id.buttonSubtraction) {
-            textView.setText("-");
-        } else if (id == R.id.buttonAddition) {
-            textView.setText("+");
-        } else if (id == R.id.buttonEqual) {
-            textView.setText("=");
-        }
+        Runnable function = functions.get(view.getId());
+        if (Objects.nonNull(function)) function.run();
+        textView.setText(String.format("%f", numerator / denominator));
+    }
 
+    private void appendNumber(int number) {
+        numerator *= 10;
+        numerator += number;
+        if (decimal) denominator *= 10;
+    }
+
+    private void setDecimal() {
+        decimal = true;
+    }
+
+    private void percent() {
+        numerator /= 100;
+    }
+
+    private void clear() {
+        numerator = 0;
+        denominator = 1;
+        decimal = false;
+    }
+
+    private void changeSign() {
+        numerator *= -1;
     }
 }
