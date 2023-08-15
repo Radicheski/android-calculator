e36725d21f55cb033b519b05fda952531da07d66
Erik Radicheski da Silva <erik.radicheski@gmail.com>
2023-08-14 21:49:13 -0300

    Removido código morto

--- app/src/main/java/dev/radicheski/calculator/MainActivity.java
+++ app/src/main/java/dev/radicheski/calculator/MainActivity.java
@@ -8,9 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
@@ -23,14 +21,6 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView textView;
 
-    private final List<Integer> BUTTONS_IDS = List.of(
-            R.id.buttonClear, R.id.buttonSign, R.id.buttonPercent, R.id.buttonDecimal,
-            R.id.buttonDivision, R.id.buttonMultiplication,
-            R.id.buttonSubtraction, R.id.buttonAddition, R.id.buttonEqual,
-            R.id.button0, R.id.button1, R.id.button2, R.id.button3, R.id.button4,
-            R.id.button5, R.id.button6, R.id.button7, R.id.button8, R.id.button9
-    );
-
     private final Map<Integer, Runnable> functions = new HashMap<>() {{
         put(R.id.buttonClear, MainActivity.this::clear);
         put(R.id.buttonSign, MainActivity.this::changeSign);
@@ -40,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         put(R.id.buttonMultiplication, MainActivity.this::multiply);
         put(R.id.buttonSubtraction, MainActivity.this::subtract);
         put(R.id.buttonAddition, MainActivity.this::add);
+        put(R.id.buttonEqual, () -> { /* TODO */ } );
         put(R.id.button0, () -> appendNumber(0));
         put(R.id.button1, () -> appendNumber(1));
         put(R.id.button2, () -> appendNumber(2));
@@ -52,8 +43,6 @@ public class MainActivity extends AppCompatActivity {
         put(R.id.button9, () -> appendNumber(9));
     }};
 
-    private final List<Button> BUTTONS = new ArrayList<>(BUTTONS_IDS.size());
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -61,10 +50,9 @@ public class MainActivity extends AppCompatActivity {
 
         textView = (TextView) findViewById(R.id.textView);
 
-        for (int id: BUTTONS_IDS) {
+        for (int id: functions.keySet()) {
             Button button = (Button) findViewById(id);
             button.setOnClickListener(this::onClick);
-            BUTTONS.add(button);
         }
     }
 
